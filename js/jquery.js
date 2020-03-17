6b2b2a74faf7b93351b79118a79e2870b5b8e6c9
Elena4060 <itelena95@gmail.com>
2020-03-17 14:03:06 +0300

    dfg

--- js/jquery.js
+++ js/jquery.js
@@ -5,13 +5,13 @@ $(document).ready(function(){
 		if (icon.hasClass("fa-bars")) {
 			icon.removeClass("fa-bars");
 			icon.addClass("fa-times");
-			$("nav").animate({ height: "250px" }, 500);
+			$(".menu-nav").animate({ height: "250px" }, 500);
 		} else {
 			icon.removeClass("fa-times");
 			icon.addClass("fa-bars");
-			$("nav").animate({ height: "80px" }, 500);
+			$(".menu-nav").animate({ height: "80px" }, 500);
 		}
-		$("menu__nav-list").slideToggle(500);
+		$("#list").slideToggle(500);
 
 	});
 
