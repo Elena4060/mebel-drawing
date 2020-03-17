8d9a436ab473b41b646ab81c339f3650e160f94b
Elena4060 <itelena95@gmail.com>
2020-03-17 14:30:22 +0300

    sdfg

--- js/jquery.js
+++ js/jquery.js
@@ -5,11 +5,11 @@ $(document).ready(function(){
 		if (icon.hasClass("fa-bars")) {
 			icon.removeClass("fa-bars");
 			icon.addClass("fa-times");
-			$(".menu-nav").animate({ height: "250px" }, 500);
+			$(".nav").animate({ height: "10rem" }, 500);
 		} else {
 			icon.removeClass("fa-times");
 			icon.addClass("fa-bars");
-			$(".menu-nav").animate({ height: "80px" }, 500);
+			$(".nav").animate({ height: "80px" }, 500);
 		}
 		$("#list").slideToggle(500);
 
